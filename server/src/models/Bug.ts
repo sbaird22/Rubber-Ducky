04896ebd64d07f187696df54451ed31ebc8082b2
this-is-yosuke <yosuke.kibe@gmail.com>
2025-02-13 20:44:26 -0700

    Created the bug model
    Added the interface. Made Attempt a subdocument. Added the schema.

---
+++ server/src/models/Bug.ts
@@ -0,0 +1,67 @@
+import { Schema, model, Document } from 'mongoose';
+
+// Attempt subdocument Interface
+interface IAttempt extends Document {
+    description: string,
+    isSuccess: boolean,
+    createdAt: Date,
+};
+
+// Bug interface
+interface IBug extends Document {
+    title: string,
+    description: string,
+    createdAt: Date,
+    createdBy: string,
+    attempts: IAttempt[],
+}
+
+// Defining the schema for the Attempt subdocument
+const attemptSchema = new Schema<IAttempt>(
+    {
+        description: {
+            type: String,
+            required: true,
+            minLength: 10,
+            maxLength: 300,
+        },
+        isSuccess: {
+            type: Boolean,
+            required: true,
+        },
+        _id: false,
+        toJSON: { getters: true},
+        toObject: { getters: true },
+        timestamps: true,
+    }
+);
+
+// Schema for Bug
+const bugSchema = new Schema<IBug>(
+    {
+        title: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 25,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            minLength: 10,
+            maxLength: 400,
+            trim: true,
+        },
+        createdBy: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        attempts: [attemptSchema],
+    },
+);
+
+const Bug = model<IBug>('Bug', bugSchema);
+
+export default Bug;
