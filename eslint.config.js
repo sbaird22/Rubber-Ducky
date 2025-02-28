0e1b41c68d0fe94d6a7848b59103e19ff199deb3
DDeMarois <Drew.DeMarois@du.edu>
2025-02-27 19:44:54 -0700

    changed eslint.config.js, jsx to tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 
@@ -6,21 +8,31 @@ export default [
     ignores: ["node_modules", "dist"],
   },
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json", // Ensure you have a valid tsconfig.json
+        tsconfigRootDir: process.cwd(),
+        ecmaFeatures: {
+          jsx: true, // Enable JSX parsing
+        },
+      },
     },
     plugins: {
+      "@typescript-eslint": ts,
       react,
       "react-hooks": reactHooks,
     },
     settings: {
       react: {
-        version: "detect", // Auto-detect React version
+        version: "detect",
       },
     },
     rules: {
+      ...ts.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react/jsx-uses-react": "error",
