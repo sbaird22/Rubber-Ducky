ab904235a3269287f16e0a3ef00bad2db53547bf
this-is-yosuke <yosuke.kibe@gmail.com>
2025-03-01 18:05:50 -0700

    Refactored the server file
    changed the path for app.use and app.get

--- server/src/server.ts
+++ server/src/server.ts
@@ -49,9 +49,10 @@ const startApolloServer = async () => {
   server.applyMiddleware({ app });
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.resolve(__dirname, '../../client/dist')));
+
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
     });
   }
 
